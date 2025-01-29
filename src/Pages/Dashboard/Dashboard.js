7e6a2abde2ed910deba86f602777b8c403cf87bc
rajneesh shukla <developer.rajneeshshukla@gmail.com>
2025-01-29 22:15:39 +0530

    table added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,18 +1,20 @@
 import React from "react";
 import CountUp from "react-countup";
 import "./dashboard.css";
+import ResponsiveTableWithPagination from "../../Components/Table/Table";
 
 function Dashboard() {
   return (
     <div className="container">
       <div className="row">
-        {[45686, 145794, 789458, 475896, 458796, 488565,434,3434,3434,343,43,43,43,43,4].map((value, index) => (
+        {[45686, 145794, 789458, 475896, 458796, 488565,].map((value, index) => (
           <div key={index} className="col-12 col-sm-6 col-lg-4 mb-3">
             <div className="dashboard-card">
               <CountUp className="counter-number" start={0} end={value} duration={4} />
             </div>
           </div>
         ))}
+        <ResponsiveTableWithPagination/>
       </div>
     </div>
   );
